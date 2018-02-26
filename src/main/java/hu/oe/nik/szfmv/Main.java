3ba7b7675f8ef5162cf8d2da2f11e9f66b24ef96
pintergreg <pintergreg@gmail.com>
2018-02-26 09:32:08 +0100

    Log the current debug mode

--- src/main/java/hu/oe/nik/szfmv/Main.java
+++ src/main/java/hu/oe/nik/szfmv/Main.java
@@ -1,8 +1,13 @@
 package hu.oe.nik.szfmv;
 
+import hu.oe.nik.szfmv.common.ConfigProvider;
 import hu.oe.nik.szfmv.visualization.Gui;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Main {
+    private static final Logger LOGGER = LogManager.getLogger();
+
     public static void main(String[] args) {
 
 //        SwingUtilities.invokeLater(() -> {
@@ -10,5 +15,9 @@ public class Main {
 //            ex.setVisible(true);
 //        });
         Gui gui = new Gui();
+
+        // log the current debug mode in config
+        LOGGER.info(ConfigProvider.provide().getBoolean("general.debug"));
+
     }
 }
