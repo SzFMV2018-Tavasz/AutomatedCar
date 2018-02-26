fecbe287c3393c08fe1ac7104a2d9507f522334c
pintergreg <pintergreg@gmail.com>
2018-02-26 09:19:29 +0100

    Remove experimental gui logger

--- src/main/java/hu/oe/nik/szfmv/visualization/Gui.java
+++ src/main/java/hu/oe/nik/szfmv/visualization/Gui.java
@@ -4,18 +4,19 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import javax.swing.*;
-import javax.swing.text.BadLocationException;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
-import javax.swing.text.StyledDocument;
 import java.awt.*;
 
 
 public class Gui extends JFrame {
 
+    private static final Logger LOGGER = LogManager.getLogger();
+    private final int windowWidth = 1020;
+    private final int windowHeight = 1020;
+
     private final SimpleAttributeSet redStyle = new SimpleAttributeSet();
     private final SimpleAttributeSet whiteStyle = new SimpleAttributeSet();
-    private static final Logger LOGGER = LogManager.getLogger();
 
     public Gui() {
         StyleConstants.setForeground(redStyle, new Color(0xFF6262));
@@ -28,7 +29,7 @@ public class Gui extends JFrame {
         setTitle("AutomatedCar");
         setLocation(0, 0); // default is 0,0 (top left corner)
         addWindowListener(new GuiAdapter());
-        setPreferredSize(new Dimension(1020,700)); // inner size is 1020,700
+        setPreferredSize(new Dimension(windowWidth, windowHeight)); // inner size
         setResizable(false);
         pack();
 
@@ -45,36 +46,6 @@ public class Gui extends JFrame {
         Dashboard dashboard = new Dashboard();
         add(dashboard);
 
-        JTextPane logViewer = new JTextPane();
-        JScrollPane scrollPane = new JScrollPane(logViewer);
-        logViewer.setEditable(false);
-        logViewer.setBackground(new Color(0x44444444));
-
-
-        scrollPane.setBounds(0, 425, 250, 275);
-        scrollPane.setWheelScrollingEnabled(true);
-        dashboard.add(scrollPane);
-        StyledDocument doc = logViewer.getStyledDocument();
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-
-
-        try {
-            for (int i=0;i<20;i++)
-            doc.insertString(doc.getLength(), "message" + "\n" + String.valueOf(i), whiteStyle);
-        } catch (BadLocationException e) {
-            e.printStackTrace();
-        }
-
         setVisible(true);
-
     }
-
-//    public void addToLogViewer(String message) {
-//        try {
-//            doc.insertString(doc.getLength(), message + "\n", redStyle);
-//        } catch (BadLocationException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
