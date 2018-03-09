8484154b819305918fe02fb2cc8153d09d120396
bench1947 <bench1947@hotmail.com>
2018-03-09 18:14:13 +0100

    Road signs can now be displayed on the dashboard
    - Added displayRoadSign(RoadSign roadSign) method to display a road sign on the newly created road sign panel.

--- src/main/java/hu/oe/nik/szfmv/visualization/Dashboard.java
+++ src/main/java/hu/oe/nik/szfmv/visualization/Dashboard.java
@@ -1,7 +1,14 @@
 package hu.oe.nik.szfmv.visualization;
 
+import hu.oe.nik.szfmv.environment.models.RoadSign;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Dashboard shows the state of the ego car, thus helps in debugging.
@@ -10,26 +17,68 @@ public class Dashboard extends JPanel {
 
     private final int width = 250;
     private final int height = 700;
+    private final int dashboardBoundsX = 770;
+    private final int dashboardBoundsY = 0;
     private final int backgroundColor = 0x888888;
+
+    private final int roadSignPanelX = 120;
+    private final int roadSignPanelY = 280;
+    private final int roadSignPanelWidth = 115;
+    private final int roadSignPanelHeight = 115;
+    private final JPanel roadSignPanel = new JPanel();
+    private final JLabel roadSignIcon = new JLabel();
+    private final JLabel roadSignLabel = new JLabel();
 
     /**
      * Initialize the dashboard
      */
     public Dashboard() {
-        InitializeDashboard();
+        initializeDashboard();
     }
 
     /**
      * Update the displayed values
      */
-    public void UpdateDisplayedValues() {
-
+    public void updateDisplayedValues() {
+        // TODO Update the road sign as well once the road sign detection is added.
     }
 
-    private void InitializeDashboard() {
+    /**
+     * Initializes the dashboard components
+     */
+    private void initializeDashboard() {
         // Not using any layout manager, but fixed coordinates
         setLayout(null);
         setBackground(new Color(backgroundColor));
-        setBounds(770, 0, width, height);
+        setBounds(dashboardBoundsX, dashboardBoundsY, width, height);
+
+        initializeRoadSignPanel();
+    }
+
+    /**
+     * Initializes the road sign panel on the dashboard
+     */
+    private void initializeRoadSignPanel() {
+        roadSignPanel.setBounds(roadSignPanelX, roadSignPanelY, roadSignPanelWidth, roadSignPanelHeight);
+        roadSignPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        roadSignPanel.add(roadSignLabel);
+        roadSignPanel.add(roadSignIcon);
+        roadSignLabel.setText("last road sign");
+        add(roadSignPanel);
+    }
+
+    /**
+     * Displays the given road sign in place of the last seen road sign.
+     * @param roadSign The last seen road sign
+     */
+    private void displayRoadSign(RoadSign roadSign) {
+        try {
+            File roadSignImageFile = new File(Objects.requireNonNull(getClass().getClassLoader().
+                    getResource(roadSign.getImageFileName())).getFile());
+            BufferedImage roadSignPicture = ImageIO.read(roadSignImageFile);
+            roadSignIcon.setIcon(new ImageIcon(roadSignPicture));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
