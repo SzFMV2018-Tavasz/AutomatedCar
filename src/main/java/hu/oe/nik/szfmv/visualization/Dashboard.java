5b19b2bf2798bcc86d7519e6cf39cc3c52b0a0c0
Modizoli <36445919+Modizoli@users.noreply.github.com>
2018-03-09 22:48:29 +0100

    ACCpanel initialization
    ACC-state-panel initialization and fake,fix data values to view

--- src/main/java/hu/oe/nik/szfmv/visualization/Dashboard.java
+++ src/main/java/hu/oe/nik/szfmv/visualization/Dashboard.java
@@ -12,6 +12,19 @@ public class Dashboard extends JPanel {
     private final int height = 700;
     private final int backgroundColor = 0x888888;
 
+    private final JPanel accStatePanel=new JPanel();
+    private final Color accStatePanelBackgroundColor=Color.white;
+    private final int accStatePanelX=25;
+    private final int accStatePanelY=400;
+    private final int accStatePanelWidth=150;
+    private final int accStatePanelHeight=50;
+
+    private final JLabel accTargetDistanceLabel = new JLabel();
+    private final JLabel accTargetSpeedLabel = new JLabel();
+    private final JLabel accDistanceLabel = new JLabel();
+    private final JLabel accSpeedLabel = new JLabel();
+
+
     /**
      * Initialize the dashboard
      */
@@ -31,5 +44,32 @@ public class Dashboard extends JPanel {
         setLayout(null);
         setBackground(new Color(backgroundColor));
         setBounds(770, 0, width, height);
+
+        InitializeAccStatePanel();
+    }
+
+    /**
+     +     * Initializes the ACC-state-panel and the labels to write the values on the dashboard
+     +
+     */
+    private void InitializeAccStatePanel() {
+        accStatePanel.setBackground(accStatePanelBackgroundColor);
+        accStatePanel.setBounds(
+                accStatePanelX,
+                accStatePanelY,
+                accStatePanelWidth,
+                accStatePanelHeight
+        );
+
+        accTargetDistanceLabel.setText("Target distance:");
+        accDistanceLabel.setText("20");
+        accTargetSpeedLabel.setText("Target speed:");
+        accSpeedLabel.setText("20");
+
+        add(accStatePanel);
+        accStatePanel.add(accTargetDistanceLabel);
+        accStatePanel.add(accDistanceLabel);
+        accStatePanel.add(accTargetSpeedLabel);
+        accStatePanel.add(accSpeedLabel);
     }
 }
