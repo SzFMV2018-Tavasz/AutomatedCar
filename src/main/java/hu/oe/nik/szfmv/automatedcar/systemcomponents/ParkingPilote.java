1cf755596390117c84b0110a3dfb69e191e731df
Gabo Petrovics <ggaborr1@gmail.com>
2018-03-12 17:43:57 +0100

    ParkingPiloteKeydown Added

---
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/systemcomponents/ParkingPilote.java
@@ -0,0 +1,37 @@
+package hu.oe.nik.szfmv.automatedcar.systemcomponents;
+
+import hu.oe.nik.szfmv.automatedcar.bus.VirtualFunctionBus;
+import hu.oe.nik.szfmv.automatedcar.bus.packets.input.InputPacket;
+import hu.oe.nik.szfmv.automatedcar.input.InputHandler;
+
+public class ParkingPilote extends SystemComponent
+{
+    private InputHandler inputhandler;
+    private final InputPacket packet;
+    private boolean on;
+
+    public ParkingPilote(VirtualFunctionBus bus)
+    {
+        super(bus);
+        on = false;
+        this.packet = InputPacket.getInstance();
+        bus.inputPacket = packet;
+        inputhandler = InputHandler.getInstance();
+    }
+
+    @Override
+    public void loop()
+    {
+        if (inputhandler.isParkinPilotePressed() && !on)
+        {
+            on = true;
+        }
+
+        if (!inputhandler.isParkinPilotePressed() && on)
+        {
+            on = false;
+        }
+
+        packet.setParkingPiloteStatus(on);
+    }
+}
