526137b6f83a9e917a54030532ce84c2ec9b8a6e
albert_hajdu_oe <albert.hajdu@hotmail.hu>
2018-03-19 20:20:58 +0100

    Failed test correction after coverage increase

--- src/test/java/hu/oe/nik/szfmv/environment/models/PedestrianTest.java
+++ src/test/java/hu/oe/nik/szfmv/environment/models/PedestrianTest.java
@@ -10,20 +10,20 @@ import static org.junit.Assert.assertTrue;
 public class PedestrianTest {
     @Test
     public void isMovable() {
-        Pedestrian pedestrian = new Pedestrian(500, 500, "man.png", 1000, 1000);
+        Pedestrian pedestrian = new Pedestrian(500, 500, "man.png");
         assertTrue(Movable.class.isInstance(pedestrian));
     }
 
     @Test
     public void moveOnCrosswalkOneTimes() {
-        Pedestrian pedestrian = new Pedestrian(500, 500, "man.png", 1000, 1000);
+        Pedestrian pedestrian = new Pedestrian(500, 500, "man.png");
         pedestrian.moveOnCrosswalk();
         assertThat(pedestrian.getY(), is(495));
     }
 
     @Test
     public void moveOnCrosswalkTenTimes() {
-        Pedestrian pedestrian = new Pedestrian(500, 500, "man.png", 1000, 1000);
+        Pedestrian pedestrian = new Pedestrian(500, 500, "man.png");
         for (int i = 0; i < 10; i++) {
             pedestrian.moveOnCrosswalk();
         }
@@ -32,7 +32,7 @@ public class PedestrianTest {
 
     @Test
     public void moveOnCrosswalkHundredTimes() {
-        Pedestrian pedestrian = new Pedestrian(500, 500, "man.png", 1000, 1000);
+        Pedestrian pedestrian = new Pedestrian(500, 500, "man.png");
         for (int i = 0; i < 100; i++) {
             pedestrian.moveOnCrosswalk();
         }
