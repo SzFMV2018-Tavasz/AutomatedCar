1443650497317acd777fdc048ea6bb94f29a8bb2
Nyerges David <nydavid95@hotmail.com>
2018-03-21 22:25:26 +0100

    Checkstyle fix

--- src/main/java/hu/oe/nik/szfmv/detector/classes/Triangle.java
+++ src/main/java/hu/oe/nik/szfmv/detector/classes/Triangle.java
@@ -1,6 +1,6 @@
 package hu.oe.nik.szfmv.detector.classes;
 
-import static java.lang.Math.*;
+import java.lang.*;
 import java.awt.Point;
 
 /**
@@ -9,25 +9,26 @@ import java.awt.Point;
 public class Triangle {
     /**
      * @param start - Start point of the sensor (Point)
-     * @param vector_Length -view range of the sensor (Double)
+     * @param vectorLength -view range of the sensor (Double)
      * @param degree - Angle of view in degrees (Double)
-     * @param sensor_rotation - in degrees (Double)
+     * @param sensorRotation - in degrees (Double)
      * @return a Point array with 3 elemtns. First element: start point, second element: A point, third element: B
      */
-    public static Point[] Triangle_points(Point start, Double vector_Length, Double degree, Double sensor_rotation) {
-        Double new_length = vector_Length / cos(toRadians(degree / 2));
-        System.out.print(cos(toRadians(degree / 2)));
+    public static Point[] TrianglePoints(Point start, Double vectorLength, Double degree, Double sensorRotation) {
+        Double newLength = vectorLength / Math.cos(Math.toRadians(degree / 2));
 
-        Point A = Get_polar_point(new_length,  90 + degree / 2 + sensor_rotation);
-        Point B = Get_polar_point(new_length, 90 - degree / 2 + sensor_rotation);
+        Double utilAngle = 90.0;
+        Point APoint = GetPolarPoint(newLength, utilAngle + degree / 2 + sensorRotation);
+        Point BPoint = GetPolarPoint(newLength, utilAngle- degree / 2 + sensorRotation);
 
-        A = Move_point(A, start);
-        B = Move_point(B, start);
+        APoint = MovePoint(APoint, start);
+        BPoint = MovePoint(BPoint, start);
 
-        Point[] triangle = new Point[3];
+        int numberOfPoints = 3;
+        Point[] triangle = new Point[numberOfPoints];
         triangle[0] = start;
-        triangle[1] = A;
-        triangle[2] = B;
+        triangle[1] = APoint;
+        triangle[2] = BPoint;
         return triangle;
     }
 
@@ -36,18 +37,18 @@ public class Triangle {
      * @param degree - angle of view in degrees
      * @return A point from polar coordinate system
      */
-    private static Point Get_polar_point(Double length, Double degree) {
-        Double x = (length * cos(toRadians(degree)));
-        Double y = (length * sin(toRadians(degree)));
-        return new Point((int)round(x.doubleValue()), (int)round(y.doubleValue()));
+    private static Point GetPolarPoint(Double length, Double degree) {
+        Double x = (length * Math.cos(Math.toRadians(degree)));
+        Double y = (length * Math.sin(Math.toRadians(degree)));
+        return new Point((int)Math.round(x.doubleValue()), (int)Math.round(y.doubleValue()));
     }
 
     /**
-     * @param original_point - point in polar coordinate system
-     * @param transformation_point - new origo
+     * @param originalPoint - point in polar coordinate system
+     * @param transformationPoint - new origo
      * @return the original point with moved coordinates
      */
-    private static Point Move_point(Point original_point, Point transformation_point) {
-        return new Point(original_point.x + transformation_point.x, original_point.y + transformation_point.y);
+    private static Point MovePoint(Point originalPoint, Point transformationPoint) {
+        return new Point(originalPoint.x + transformationPoint.x, originalPoint.y + transformationPoint.y);
     }
 }
