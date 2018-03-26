54c8d59cc2eeabcb4b8a3940af9c2a4cba5a583c
Skornyák András <imtherealandrew@gmail.com>
2018-03-26 18:59:44 +0200

    javitas

--- src/main/java/hu/oe/nik/szfmv/automatedcar/systemcomponents/SensorsVisualizer.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/systemcomponents/SensorsVisualizer.java
@@ -1,11 +1,10 @@
 package hu.oe.nik.szfmv.automatedcar.systemcomponents;
 
-import com.sun.media.jfxmedia.logging.Logger;
 import hu.oe.nik.szfmv.automatedcar.bus.VirtualFunctionBus;
 import hu.oe.nik.szfmv.automatedcar.bus.packets.input.InputPacket;
 import hu.oe.nik.szfmv.automatedcar.input.InputHandler;
 
-import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
+
 
 public class SensorsVisualizer extends SystemComponent {
 
