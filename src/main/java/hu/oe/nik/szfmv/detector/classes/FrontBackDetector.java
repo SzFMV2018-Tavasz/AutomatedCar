f74c08bf1d2e4fb651e4eea45aed840a0e85cd41
Peter Romsics <peter.romsics@gmail.com>
2018-03-31 18:42:54 +0200

    reformatted code

--- src/main/java/hu/oe/nik/szfmv/detector/classes/FrontBackDetector.java
+++ src/main/java/hu/oe/nik/szfmv/detector/classes/FrontBackDetector.java
@@ -1,4 +1,5 @@
 package hu.oe.nik.szfmv.detector.classes;
+
 import hu.oe.nik.szfmv.environment.WorldObject;
 import hu.oe.nik.szfmv.environment.models.Collidable;
 
@@ -16,10 +17,10 @@ public class FrontBackDetector extends Detector {
         previousCollidables = new ArrayList<Collidable>();
     }
 
-    private Point[] createCenterLine(Polygon Triangle){
+    private Point[] createCenterLine(Polygon Triangle) {
         Point[] p = new Point[2];
-        p[0] = new Point(Triangle.xpoints[0],Triangle.ypoints[0]);
-        p[1] = new Point((Triangle.xpoints[1] + Triangle.xpoints[2])/2,(Triangle.ypoints[1] + Triangle.ypoints[2])/2);
+        p[0] = new Point(Triangle.xpoints[0], Triangle.ypoints[0]);
+        p[1] = new Point((Triangle.xpoints[1] + Triangle.xpoints[2]) / 2, (Triangle.ypoints[1] + Triangle.ypoints[2]) / 2);
 
         return p;
     }
@@ -28,20 +29,20 @@ public class FrontBackDetector extends Detector {
         return previousCollidables;
     }
 
-    List<Collidable> getCollidableObjectsApproachingCenterLineofAutomatedCar (Point[] CenterLine, ArrayList<Collidable> CollidableObjectsInTriangle) {
+    List<Collidable> getCollidableObjectsApproachingCenterLineofAutomatedCar(Point[] CenterLine, ArrayList<Collidable> CollidableObjectsInTriangle) {
         ArrayList<Collidable> ApproachingCollidables = new ArrayList<Collidable>();
 
         for (Collidable object : CollidableObjectsInTriangle) {
 
             Collidable PreviousCollidable = null;
             for (Collidable previousobject : previousCollidables) {
-                if (previousobject.getImageFileName() == object.getImageFileName() && 1==1) {
+                if (previousobject.getImageFileName() == object.getImageFileName() && 1 == 1) {
                     PreviousCollidable = previousobject;
                     break;
                 }
             }
 
-            if(PreviousCollidable == null || pointToLineDistance(CenterLine[0],CenterLine[1],object.getLocation()) < pointToLineDistance(CenterLine[0],CenterLine[1],PreviousCollidable.getLocation())){
+            if (PreviousCollidable == null || pointToLineDistance(CenterLine[0], CenterLine[1], object.getLocation()) < pointToLineDistance(CenterLine[0], CenterLine[1], PreviousCollidable.getLocation())) {
                 ApproachingCollidables.add(object);
             }
         }
@@ -51,7 +52,7 @@ public class FrontBackDetector extends Detector {
     }
 
     private double pointToLineDistance(Point A, Point B, Point P) {
-        double normalLength = Math.sqrt((B.x-A.x)*(B.x-A.x)+(B.y-A.y)*(B.y-A.y));
-        return Math.abs((P.x-A.x)*(B.y-A.y)-(P.y-A.y)*(B.x-A.x))/normalLength;
+        double normalLength = Math.sqrt((B.x - A.x) * (B.x - A.x) + (B.y - A.y) * (B.y - A.y));
+        return Math.abs((P.x - A.x) * (B.y - A.y) - (P.y - A.y) * (B.x - A.x)) / normalLength;
     }
 }
