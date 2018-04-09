a5ba829149985caeaff53a0098d24d440c98c0bd
balint.bekefi <balint.bekefi@gmail.com>
2018-04-10 01:18:45 +0200

    Started working on displaying sensor triangles for debugging -- only radar yet, rotation does not work (why?)

--- src/main/java/hu/oe/nik/szfmv/Main.java
+++ src/main/java/hu/oe/nik/szfmv/Main.java
@@ -42,14 +42,14 @@ public class Main {
         Gui gui = new Gui();
 
         // draw world to course display
-        gui.getCourseDisplay().drawWorld(w, car.getCarValues());
+        gui.getCourseDisplay().drawWorld(w, car.getCarValues(), car.getInputValues());
 
         while (true) {
             try {
                 car.drive();
                 pedestrian.moveOnCrosswalk();
 
-                gui.getCourseDisplay().drawWorld(w, car.getCarValues());
+                gui.getCourseDisplay().drawWorld(w, car.getCarValues(), car.getInputValues());
                 gui.getDashboard().updateDisplayedValues(car.getInputValues(), car.getPowertrainValues(),
                         car.getX(), car.getY());
                 Thread.sleep(CYCLE_PERIOD);
