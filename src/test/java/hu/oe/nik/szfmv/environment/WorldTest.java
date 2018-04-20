64e7028f9762bfd8bc146a562ecddbeced874fee
roba <varga.robert2@gmail.com>
2018-04-20 15:15:24 +0200

    fix WorldTest

--- src/test/java/hu/oe/nik/szfmv/environment/WorldTest.java
+++ src/test/java/hu/oe/nik/szfmv/environment/WorldTest.java
@@ -86,7 +86,7 @@ public class WorldTest {
     public void isGameOver() {
         World testWorld = new World(0, 0);
         AutomatedCar car = new AutomatedCar(199, 90, "car_2_blue.png");
-        RoadSign roadSign = new RoadSign(201, 180,"roadsign_speed_40.png");
+        RoadSign roadSign = new RoadSign(85, 35,"roadsign_speed_40.png");
         LOGGER.debug("isGameOver method::Car: " + car.getShape().getBounds());
         LOGGER.debug("isGameOver method::RoadSign: " + roadSign.getShape().getBounds());
 
@@ -98,7 +98,7 @@ public class WorldTest {
         Assert.assertTrue(testWorld.isColliding(car, roadSign));
         Assert.assertFalse(testWorld.isGameOver());
 
-        Pedestrian pedestrian = new Pedestrian(200, 180, "man.png");
+        Pedestrian pedestrian = new Pedestrian(85, 35, "man.png");
         LOGGER.debug("isGameOver method::Pedestrian: " + pedestrian.getShape().getBounds());
         testWorld.addObjectToWorld(pedestrian);
 
@@ -111,7 +111,7 @@ public class WorldTest {
     public void isColliding() {
         World testWorld = new World(0, 0);
         AutomatedCar car = new AutomatedCar(202, 94, "car_2_white.png");
-        Tree tree = new Tree(205, 180, "tree.png");
+        Tree tree = new Tree(110, 35, "tree.png");
 
         LOGGER.debug("isColliding method::Car: " + car.getShape().getBounds());
         LOGGER.debug("isColliding method::Tree1: " + tree.getShape().getBounds());
