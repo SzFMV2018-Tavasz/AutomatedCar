63474edc5f2e936a235ba232bd413e70ed9fe124
roba <varga.robert2@gmail.com>
2018-04-21 11:27:31 +0200

    some checkstyle fix

--- src/main/java/hu/oe/nik/szfmv/automatedcar/bus/packets/reverseradar/ReadOnlyReverseRadarPacket.java
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/bus/packets/reverseradar/ReadOnlyReverseRadarPacket.java
@@ -6,18 +6,21 @@ public interface ReadOnlyReverseRadarPacket {
 
     /**
      * Gets distance between back of the car and its near object
+     *
      * @return distance
      */
     double getDistance();
 
     /**
      * Gets the Reverse Radar State (distance > 0.8: OK; distance < 0.8: WARNING; distance < 0.4: DANGER
+     *
      * @return state
      */
     ReverseRadarState getReverseRadarState();
 
     /**
      * Gets the Reverse Radar is activated or not
+     *
      * @return Activation status
      */
     Boolean getActivation();
