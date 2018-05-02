7b0bdb8162d1df332d57b62e5c98a55d89901204
roba <varga.robert2@gmail.com>
2018-05-02 16:30:07 +0200

    refactoring powertrain package's place, and some checkstyle issue

---
+++ src/main/java/hu/oe/nik/szfmv/automatedcar/bus/packets/powertrain/PowertrainTestPacket.java
@@ -0,0 +1,44 @@
+package hu.oe.nik.szfmv.automatedcar.bus.packets.powertrain;
+
+import hu.oe.nik.szfmv.automatedcar.input.enums.GearEnum;
+
+public class PowertrainTestPacket implements ReadOnlyPowertrainTestPacket {
+    private int gaspedalPosition;
+    private int brakepedalPosition;
+    private GearEnum gearState;
+
+    /**
+     * PowertrainTestPacket constructor
+     */
+    public PowertrainTestPacket() {
+        gaspedalPosition = 0;
+        brakepedalPosition = 0;
+        this.gearState = GearEnum.P;
+    }
+
+    public int getGaspedalPosition() {
+        return this.gaspedalPosition;
+    }
+
+    public void setGaspedalPosition(int gaspedalPosition) {
+        this.gaspedalPosition = gaspedalPosition;
+    }
+
+    @Override
+    public int getBrakepedalPosition() {
+        return brakepedalPosition;
+    }
+
+    public void setBrakepedalPosition(int brakepedalPosition) {
+        this.brakepedalPosition = brakepedalPosition;
+    }
+
+    @Override
+    public GearEnum getGearState() {
+        return gearState;
+    }
+
+    public void setGearState(GearEnum gearState) {
+        this.gearState = gearState;
+    }
+}
